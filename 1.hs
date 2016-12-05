8ad46cd73b29a8fe78e3a580efca773034dc5a8c
elsie <elimohl@gmail.com>
2016-12-05 13:35:37 +0300

    Better names

--- 1.hs
+++ 1.hs
@@ -8,21 +8,21 @@ import Data.Char
 just2Int (Just n) = n
 just2Int Nothing = 0
 
-sym2Int (c) =
+hexSym2Int (c) =
     just2Int $ elemIndex c (['0'..'9'] ++ ['a'..'z'])
 
-hex2Dec (s) =
-     foldl (\a b -> a * 16  + b) 0 $ map sym2Int s
+hex2Int (s) =
+     foldl (\a b -> a * 16  + b) 0 $ map hexSym2Int s
 
 int2Sym (c) =
        (['A'..'z'] ++ ['0'..'9'] ++ ['!', '@']) !! c
 
-hex2Chr (c) =
-    chr $ hex2Dec c
+hexByte2Chr (c) =
+    chr $ hex2Int c
 
 hex2Str "" = ""
 hex2Str (s) =
-    hex2Chr (take 2 s) : hex2Str (drop 2 s)
+    hexByte2Chr (take 2 s) : hex2Str (drop 2 s)
 
 main = do
     line <- getLine
