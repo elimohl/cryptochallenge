210c54d5fe0383f30b5272ab09c0984c9c26f920
elsie <elimohl@gmail.com>
2016-12-06 00:51:10 +0300

    Delete usless parenthesis

--- 1.hs
+++ 1.hs
@@ -11,27 +11,25 @@ just2Int (Just n) = n
 just2Int Nothing = 0
 
 hexSym2Int :: Char -> Int 
-hexSym2Int (c) =
+hexSym2Int c =
     just2Int $ elemIndex c (['0'..'9'] ++ ['a'..'f'])
 
 hex2Int :: String -> Int 
-hex2Int (s) =
+hex2Int s =
      foldl (\a b -> a * 16  + b) 0 $ map hexSym2Int s
 
 int2Sym :: Int -> Char
-int2Sym (c) =
+int2Sym c =
        (['A'..'z'] ++ ['0'..'9'] ++ ['!', '@']) !! c
 
 hexByte2Chr :: String -> Char
-hexByte2Chr (c) =
+hexByte2Chr c =
     chr $ hex2Int c
 
 hex2Str :: String -> String
 hex2Str "" = ""
-hex2Str (s) =
+hex2Str s =
     hexByte2Chr (take 2 s) : hex2Str (drop 2 s)
-
-
 
 main = do
     line <- getLine
