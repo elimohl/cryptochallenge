15d1dbe3f19792eadfe26c5ea5c6af2c9966c94b
elsie <elimohl@gmail.com>
2016-12-06 23:31:48 +0300

    Convertion to Base64 now works

--- 1.hs
+++ 1.hs
@@ -20,7 +20,7 @@ hex2Int s =
 
 int2Sym :: Int -> Char
 int2Sym c =
-       (['A'..'z'] ++ ['0'..'9'] ++ ['!', '@']) !! c
+       (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ ['!', '@']) !! c
 
 hexByte2Chr :: String -> Char
 hexByte2Chr c =
@@ -31,6 +31,29 @@ hex2Str "" = ""
 hex2Str s =
     hexByte2Chr (take 2 s) : hex2Str (drop 2 s)
 
+ints2Bytes :: [Int] -> Int
+ints2Bytes m =
+    shiftL (m !! 0) 16 + shiftL (m !! 1) 8 + m !! 2
+
+bytes2Base64 :: Int -> String
+bytes2Base64 0 = ""
+bytes2Base64 n =
+    (bytes2Base64 $ shiftR n 6) ++ [int2Sym $ n .&. 63]
+
+ints2Base64 :: [Int] -> String
+ints2Base64 m =
+    (take n $ bytes2Base64 $ ints2Bytes $ m ++ replicate (3 - length m) 0) ++ replicate (4 - n) '=' where n = div ((length m) * 4) 3
+
+chrs2Base64 :: String -> String
+chrs2Base64 s =
+    ints2Base64 $ map ord s
+
+str2Base64 :: String -> String
+str2Base64 "" = ""
+str2Base64 s =
+    chrs2Base64 (take 3 s) ++ str2Base64 (drop 3 s)
+
 main = do
     line <- getLine
     putStrLn $ hex2Str line
+    putStrLn $ str2Base64 $ hex2Str line
